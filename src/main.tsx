a4cf0a1a9c288a7e7fb68ad184ccd3d9885a0625
joanakar3dmoon <joanlazaro83@gmail.com>
2026-07-16 20:24:58 +0200

    fix: añadir import React en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { StrictMode, Component, type ReactNode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
